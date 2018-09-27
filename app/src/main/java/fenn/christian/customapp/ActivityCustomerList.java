d46300a2a4c0e909f4e19cc24c70b6ef9ec2f8eb
BuildTools <unconfigured@null.spigotmc.org>
2018-09-27 02:06:37 -0400

    assignment 5

---
+++ app/src/main/java/fenn/christian/customapp/ActivityCustomerList.java
@@ -0,0 +1,80 @@
+package fenn.christian.customapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class ActivityCustomerList extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.customer_list);
+
+        // ################# Fragments ##################### //
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment header = fm.findFragmentById(R.id.header_fragment);
+        Fragment customerList = fm.findFragmentById(R.id.customer_list_fragment);
+
+        // check if header exists within container
+        // if not create an instance of the fragment and add it to the container
+        if (header == null) {
+            header = new FragmentHeader();
+            fm.beginTransaction()
+                    .add(R.id.header_container, header)
+                    .commit();
+        }
+
+        if (customerList == null) {
+            customerList = new FragmentCustomerList();
+            fm.beginTransaction()
+                    .add(R.id.customer_list_container, customerList)
+                    .commit();
+        }
+
+        // ############# Floating Action Menu ############### //
+
+        FloatingActionButton logout = (FloatingActionButton) findViewById(R.id.action_logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.app.FragmentManager manager = getFragmentManager();
+                FragmentLogout dialog = new FragmentLogout();
+                dialog.show(manager, "Logout");
+            }
+        });
+
+        final FloatingActionButton newCustomer = (FloatingActionButton)
+                findViewById(R.id.action_new_customer);
+        newCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Customer customer = new Customer();
+                Bundle myBundle = new Bundle();
+                myBundle.putSerializable(ApplicationKeys.CUSTOMER_ID, customer.getCustomerId());
+                Intent newCustomerIntent = new Intent(ActivityCustomerList.this,
+                        NewCustomer.class);
+                newCustomerIntent.putExtras(myBundle);
+                ActivityCustomerList.this.startActivity(newCustomerIntent);
+            }
+        });
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+
+        finish();
+        startActivity(getIntent());
+    }
+}
---
+++ app/src/main/java/fenn/christian/customapp/CustomerCursorWrapper.java
@@ -0,0 +1,41 @@
+package fenn.christian.customapp;
+
+import android.database.Cursor;
+import android.database.CursorWrapper;
+
+import fenn.christian.customapp.Customer;
+
+
+import java.util.Date;
+import java.util.UUID;
+
+public class CustomerCursorWrapper extends CursorWrapper {
+    public CustomerCursorWrapper (Cursor cursor) {
+        super(cursor);
+    }
+
+    public Customer getCustomer() {
+        String customerIdString = getString(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.CUSTOMER_ID));
+        String firstNameString = getString(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.FIRST_NAME));
+        String lastNameString = getString(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.LAST_NAME));
+        long birthdateLong = getLong(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.BIRTHDATE));
+        String billingString = getString(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.BILLING_INFO));
+        String emailString = getString(getColumnIndex(
+                TrainerDatabase.CustomerTable.Columns.EMAIL));
+
+        Customer customer = new Customer(UUID.fromString(customerIdString));
+
+        customer.setFirstName(firstNameString);
+        customer.setLastName(lastNameString);
+        customer.setBirthDate(new Date(birthdateLong));
+        customer.setBillingInformation(billingString);
+        customer.setEmail(emailString);
+
+        return customer;
+    }
+}
