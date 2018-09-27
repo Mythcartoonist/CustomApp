accbf9bf4fd07855bd6018bfce74e54a9fb51797
BuildTools <unconfigured@null.spigotmc.org>
2018-09-26 22:44:15 -0400

    assignment 3

---
+++ app/src/main/java/fenn/christian/customapp/MainActivity.java
@@ -0,0 +1,63 @@
+package fenn.christian.customapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends FragmentActivity {
+
+    private Button mCustomerList;
+    private Button mSessionList;
+    private Button mNewCustomer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment header = fm.findFragmentById(R.id.username_textview);
+
+        if (header == null) {
+            header = new DisplayUsername();
+            fm.beginTransaction()
+                    .add(R.id.header_container, header)
+                    .commit();
+        }
+
+        mCustomerList = (Button)findViewById(R.id.listCustomersButton);
+        mCustomerList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent CustomerListIntent = new Intent(MainActivity.this, CustomerList.class);
+                MainActivity.this.startActivity(CustomerListIntent);
+            }
+        });
+
+        mSessionList = (Button)findViewById(R.id.viewCustomerSessionsButton);
+        mSessionList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent SessionListIntent = new Intent(MainActivity.this, SessionList.class);
+                MainActivity.this.startActivity(SessionListIntent);
+            }
+        });
+
+        mNewCustomer = (Button)findViewById(R.id.newCustomerButton);
+        mNewCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent NewCustomerIntent = new Intent(MainActivity.this, NewCustomer.class);
+                MainActivity.this.startActivity(NewCustomerIntent);
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/fenn/christian/customapp/SessionList.java
@@ -0,0 +1,25 @@
+package fenn.christian.customapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+public class SessionList extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_session_list);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment header = fm.findFragmentById(R.id.username_textview);
+
+        if (header == null) {
+            header = new DisplayUsername();
+            fm.beginTransaction()
+                    .add(R.id.header_container, header)
+                    .commit();
+        }
+    }
+}
